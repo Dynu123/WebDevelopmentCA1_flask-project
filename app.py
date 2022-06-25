df7d767308b4ebcd63f0d75c32a385d2b1d6aa6c
Dyana Varghese <dyna1.arackal@gmail.com>
2022-06-25 07:55:38 +0100

    added login and signup api

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import pymysql
+from werkzeug.security import check_password_hash, generate_password_hash
+import jwt
+import datetime
+
+app = Flask(__name__)
+
+#app configurations
+app.config['SECRET_KEY'] = 'thisisimportant'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://b5b7e9d2945fc0:f06fdd58@us-cdbr-east-05.cleardb.net/heroku_744c5b8a948159b"
+pymysql.install_as_MySQLdb()
+db = SQLAlchemy(app)
+
+
+#User table
+class UserTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    phone = db.Column(db.String(255), unique=True)
+    password=db.Column(db.String(255))
+    
+    def __init__(self, public_id, name, email, phone, password):
+        self.public_id = public_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.password = password
+    
+db.create_all()
+    
+
+#user registation
+@app.route('/signup', methods=['POST'])
+def createUser():
+    input = request.get_json()
+    check_user = UserTable.query.filter_by(email=input['email']).first()
+    if check_user:
+        return make_response({'result': {'data': {},
+                                     'message': 'User already exists',
+                                     'code': '400'}
+                          }, 
+                         200)
+    hashed_password = generate_password_hash(input['password'], method='sha256')
+    new_user = UserTable(str(uuid.uuid4()), input['name'], input['email'], input['phone'], hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return make_response({'result': {'data': {'name': new_user.name,
+                                              'email': new_user.email,
+                                              'phone': new_user.phone},
+                                     'message': 'User created successfully!',
+                                     'code': '200'}
+                          }, 
+                         200)
+    
+
+#user login
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if email and password:
+        get_user = UserTable.query.filter_by(email=email).first()
+        if get_user:
+            if check_password_hash(get_user.password, password):
+                token = jwt.encode({'public_id':get_user.public_id,
+                                    'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=30)}, 
+                                   app.config['SECRET_KEY'])
+                return make_response({'result': {'data': {'name': get_user.name,
+                                                          'email': get_user.email,
+                                                          'phone': get_user.phone,
+                                                          'token': token},
+                                                 'message': 'User logged in successfully!',
+                                                 'code': '200'}
+                          }, 
+                         200)
+            return make_response({'result': {'data': {},
+                                         'message': 'Incorrect password',
+                                         'code': '400'}
+                          }, 
+                         200)
+        return make_response({'result': {'data': {},
+                                         'message': 'User does not exist',
+                                         'code': '400'}
+                          }, 
+                         400)
+    return make_response({'result': {'data': {},
+                                         'message': 'Username or password cannot be empty',
+                                         'code': '400'}
+                          }, 
+                         400)
+        
+        
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
